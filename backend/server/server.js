ecb8f1d7b84a41388dcb6b7851ef213f6d1a79ed
Sibtain011 <158251023+Sibtain011@users.noreply.github.com>
2026-03-02 17:04:55 +0530

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // MUST be before routes
 
-app.use("/", userRoutes);
+app.use("/todos", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
